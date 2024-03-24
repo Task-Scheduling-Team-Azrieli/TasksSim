5fca150e8b0d0eb5c09382e0771599a59d106301
aviv0404 <aviv040400@gmail.com>
2024-03-24 23:53:27 +0200

    added all tasks to Algorithm, mainly in order to implement the critical path

--- Algorithms/GreedyHeuristics.py
+++ Algorithms/GreedyHeuristics.py
@@ -5,27 +5,43 @@ from Processor import Processor
 
 
 class OutDegreesFirst(Algorithm):
-    def __init__(self, ready_tasks: List["Task"], processors: List["Processor"]):
-        super().__init__(ready_tasks, processors)
+    def __init__(
+        self,
+        ready_tasks: List["Task"],
+        processors: List["Processor"],
+        all_tasks: List["Task"],
+    ):
+        super().__init__(ready_tasks, processors, all_tasks)
 
     # prioritize tasks with high amount of out-degrees
     def decide(self):
         result = sorted(self.ready_tasks, key=lambda task: -len(task.blocking))
         return result
-    
+
+
 class MinRuntimeFirst(Algorithm):
-    def __init__(self, ready_tasks: List["Task"], processors: List["Processor"]):
-        super().__init__(ready_tasks, processors)
+    def __init__(
+        self,
+        ready_tasks: List["Task"],
+        processors: List["Processor"],
+        all_tasks: List["Task"],
+    ):
+        super().__init__(ready_tasks, processors, all_tasks)
 
     # prioritize tasks with high amount of out-degrees
     def decide(self):
         result = sorted(self.ready_tasks, key=lambda task: task.duration)
         return result
-    
+
 
 class MaxRuntimeFirst(Algorithm):
-    def __init__(self, ready_tasks: List["Task"], processors: List["Processor"]):
-        super().__init__(ready_tasks, processors)
+    def __init__(
+        self,
+        ready_tasks: List["Task"],
+        processors: List["Processor"],
+        all_tasks: List["Task"],
+    ):
+        super().__init__(ready_tasks, processors, all_tasks)
 
     # prioritize tasks with high amount of out-degrees
     def decide(self):
